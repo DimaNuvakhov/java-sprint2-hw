a346b62ea781eccf052526f74759798dc2fae534
Dima Nuvakhov <d.nuvakhov16@mail.ru>
2021-12-26 15:17:55 +0300

    Добавил в вывод subTask id

--- src/Epic.java
+++ src/Epic.java
@@ -18,9 +18,9 @@ public class Epic extends Task {
         StringBuilder detail = new StringBuilder();
         String verticalTableBorder = "|";
         String horizontalTableBorder = "-------------------------------------"
-                + "---------------------------------------------------------";
+                + "---------------------------------------------------------------------------------------------";
         ;
-        String table = verticalTableBorder + Manager.padLeft("<Название>", 20) + verticalTableBorder +
+        String table = verticalTableBorder + Manager.padLeft("<id>", 35) + verticalTableBorder + Manager.padLeft("<Название>", 20) + verticalTableBorder +
                 (Manager.padLeft("<Описание>", 50) + verticalTableBorder)
                 + (Manager.padLeft("<Статус>", 20) + verticalTableBorder);
         String title = "Epic{" +
@@ -35,6 +35,8 @@ public class Epic extends Task {
 
             detail.
                     append("\n").
+                    append(verticalTableBorder).
+                    append(Manager.padLeft(tasks.getId(), 35)).
                     append(verticalTableBorder).
                     append(Manager.padLeft(tasks.getName(), 20)).
                     append(verticalTableBorder).
